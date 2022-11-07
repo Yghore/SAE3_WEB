dd3392942160b568f9c8e494d6da01b96277fbb9
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2022-11-07 14:33:51 +0100

    Dispacher changé

--- NetVod/src/classes/dispatcher/Dispatcher.php
+++ NetVod/src/classes/dispatcher/Dispatcher.php
@@ -6,23 +6,13 @@ use iutnc\netvod\action\AddUser;
 
 class Dispatcher
 {
-    public function run() : string
+    public function run()
     {
-        $action = (!empty($_GET['action'])) ? $_GET['action'] : "default";
-        $content= <<<EOF
-        <!DOCTYPE html>
-        <html lang="fr">
-        <head>
-            <meta charset="UTF-8">
-            <title>NetVod - Video Streaming</title>
-        </head>
-        <body>
-        EOF;
-
+        $action = null;
+        $html = '';
         switch ($action){
             case 'add-user':
                 $action = new AddUser();
-                $content = $action->execute();
                 break;
             case 'signin':
                 //TODO
@@ -37,12 +27,37 @@ class Dispatcher
                 //TODO
                 break;
             default:
-                //TODO
+                $html = "Bienvenue";
                 break;
         }
-
-        $content .= '</body></html>';
-
-        return $content;
+        if ($action !== null){
+            $html = $action->execute();
+        }
+        $this->renderPage($html);
     }
+
+    private function renderPage(string $html){
+        echo <<<END
+        <!DOCTYPE html>
+        <html lang = "fr">
+            <head>
+                <title>NetVod - Video Streaming</title>
+                <meta charset= "utf8" />
+            </head>
+            
+            <body>
+                <h1>NetVod - Video Streaming</h1>
+                <nav><ul>
+                    <li><a href="?action=add-user">Ajouter un utilisateur</a></li>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="?action=add-serial">ajouter une serie</a></li>
+                    <li><a href="?action=print-catalogue">afficher la categorie</a></li>
+                    <li><a href="?action=add-episode">ajouter un episode</a></li>
+                </nav></ul>
+                <h2>$html</h2>
+            </body>
+        </html>
+    END;
+    }
+    
 }
