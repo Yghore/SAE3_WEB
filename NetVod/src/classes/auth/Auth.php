6460f777be1b4fcbab3ad2f2a2440e68055a5f1a
Yghore <yhgore@gmail.com>
2022-11-07 14:06:31 +0100

    Ajout de la stucture des tables Ajout de la méthode de login (Début)

--- NetVod/src/classes/auth/Auth.php
+++ NetVod/src/classes/auth/Auth.php
@@ -3,7 +3,6 @@
 namespace iutnc\netvod\auth;
 
 use iutnc\deefy\db\ConnectionFactory;
-use MongoDB\Driver\Exception\ConnectionException;
 
 class Auth
 {
@@ -11,6 +10,7 @@ class Auth
     public static function login(string $email, string $password)
     {
         $db = ConnectionFactory::makeConnection();
+        $state = $db->prepare("SELECT email from user");
     }
 
 
