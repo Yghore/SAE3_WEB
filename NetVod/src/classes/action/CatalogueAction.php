edf5366e3a6abe99bc277c7ab934bf402febd057
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2022-11-07 20:17:03 +0100

    <3 I love u

--- NetVod/src/classes/action/CatalogueAction.php
+++ NetVod/src/classes/action/CatalogueAction.php
@@ -11,9 +11,10 @@ class CatalogueAction extends Action
     {
         $html = '';
         $pdo = ConnectionFactory::makeConnection();
-        if (! isset($_GET['titre'])){
+        if (! isset($_GET['id'])){
             $query = <<<end
             SELECT
+                id,
                 titre,
                 img
             FROM
@@ -24,37 +25,55 @@ class CatalogueAction extends Action
             $html .= '<form action="index.php?action=add-user" method="post">';
             $html .= '<nav><ul>';
             while ($row = $resultatSet->fetch()){
+                $idserie = $row['id'];
                 $titre = $row['titre'];
                 $image = $row['img'];
-                $html .= "<li><a href=\"index.php?action=print-catalogue&titre=$titre\">$titre $image</a></li>";
+                $html .= "<li><a href=\"index.php?action=print-catalogue&id=$idserie\">$titre $image</a></li>";
             }
             $html .= '</nav></ul>';
         } else {
-            $titre = $_GET['titre'];
+            $idserie = $_GET['id'];
             $query = <<<end
             SELECT
-                *
+                *,
+                COUNT(episode.serie_id) as 'nbepisodes'
             FROM
                 serie
+            INNER JOIN episode ON episode.serie_id = serie.id
             WHERE
-                titre = ?
+                serie.id = ?
             end;
             $resultatSet= $pdo->prepare($query);
-            $resultatSet->execute([$titre]);
+            $resultatSet->execute([$idserie]);
+            $serieid = '';
             while ($row = $resultatSet->fetch()){
+                $serieid = $row['id'];
                 $html .= 'titre : ' .  $row['titre'] . "<br/>";
                 $html .= 'descriptif : ' . $row['descriptif'] . "<br/>";
                 $html .= 'img : ' . $row['img'] . "<br/>";
                 $html .= 'annee : ' . $row['annee'] . "<br/>";
-                $html .= 'date d ajout' . $row['date_ajout'] . "<br/>";
+                $html .= 'date d ajout : ' . $row['date_ajout'] . "<br/>";
+                $html .= 'nombre d épisodes : ' . $row['nbepisodes'];
             }
 
             $query2 = <<<end
-
+            SELECT
+              *
+            FROM
+                episode
+            WHERE serie_id = ?
             end;
-
+            $resultatSet2 = $pdo->prepare($query2);
+            $resultatSet2->execute([$idserie]);
+            while ($row2 = $resultatSet2->fetch()){
+                $idepisode = $row2['id'];
+                $titre2 = $row2['titre'];
+                $numero2 = $row2['numero'];
+                $duree = $row2['duree'];
+                $img = '';
+                $html .= "<li><a href=\"index.php?action=print-catalogue&id=$idserie&idepisode=$idepisode\"> $numero2 $titre2 $duree $img </a></li>";
+            }
         }
-
         return $html;
     }
 
