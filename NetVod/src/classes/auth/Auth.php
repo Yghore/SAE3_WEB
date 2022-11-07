9499759a608267635c23ed23761411011e9110ce
Yghore <yhgore@gmail.com>
2022-11-07 12:48:46 +0100

    Modification Auth.php (Add login signature method)

--- NetVod/src/classes/auth/Auth.php
+++ NetVod/src/classes/auth/Auth.php
@@ -2,7 +2,16 @@
 
 namespace iutnc\netvod\auth;
 
+use iutnc\deefy\db\ConnectionFactory;
+use MongoDB\Driver\Exception\ConnectionException;
+
 class Auth
 {
+
+    public static function login(string $email, string $password)
+    {
+        $db = ConnectionFactory::makeConnection();
+    }
+
 
 }
