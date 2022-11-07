5fdc0384609e9c1ab98f2d0195366a63b5ac01a3
Vavasseur Regis <vavasseu2u@etu.univ-lorraine.fr>
2022-11-07 15:02:31 +0100

    ajout de la classe AddSerial.php

---
+++ NetVod/src/classes/action/AddSerial.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace iutnc\netvod\action;
+
+class AddSerial extends Action
+{
+
+    protected function executeGET(): string
+    {
+        // TODO: Implement executeGET() method.
+    }
+
+    protected function executePOST(): string
+    {
+        // TODO: Implement executePOST() method.
+    }
+}
