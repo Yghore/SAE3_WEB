7c7c0fe8cc26743248a433a2f0e22623189278d9
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2022-11-07 15:12:06 +0100

    dispacher et nouvelle classe

---
+++ NetVod/src/classes/action/AddEpisodeAction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace iutnc\netvod\action;
+
+class AddEpisodeAction extends Action
+{
+
+    protected function executeGET(): string
+    {
+        // TODO: Implement executeGET() method.
+    }
+
+    protected function executePOST(): string
+    {
+        // TODO: Implement executePOST() method.
+    }
+}
