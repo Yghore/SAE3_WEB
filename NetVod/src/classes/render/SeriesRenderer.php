8d6ae1e9c23baa902be022e28fe1d54a0e9a54d0
Yghore <yhgore@gmail.com>
2022-11-08 15:00:51 +0100

    Fonction 7 SerieRenderer.php SeriesRenderer.php

---
+++ NetVod/src/classes/render/SeriesRenderer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace iutnc\netvod\render;
+
+class SeriesRenderer implements Renderer
+{
+
+    protected array $series;
+
+    public function __construct(array $s)
+    {
+        $this->series = $s;
+    }
+
+
+    public function render(int $selector = 1): string
+    {
+        $html = "<div>";
+        foreach ($this->series as $value)
+        {
+            $render = new SerieRenderer($value);
+            $html .= $render->render();
+        }
+        $html .= "</div>";
+        return $html;
+
+    }
+}
