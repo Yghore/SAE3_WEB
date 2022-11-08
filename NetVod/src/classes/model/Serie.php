d39b1c90ffae0c1815bb23335bc008cdf32dcd10
Yghore <yhgore@gmail.com>
2022-11-08 12:46:11 +0100

    Ajout getSerie méthode

--- NetVod/src/classes/model/Serie.php
+++ NetVod/src/classes/model/Serie.php
@@ -2,10 +2,27 @@
 
 namespace iutnc\netvod\model;
 
+use iutnc\netvod\db\ConnectionFactory;
+
 class Serie
 {
 
-    private
+    private int $id;
+    private String $titre;
+    private String $description;
+    private String $img;
+    private int $annee;
+    private String $date;
+
+
+    public static function getSerie(int $id)
+    {
+        $db = ConnectionFactory::makeConnection();
+        $state = $db->prepare("SELECT * FROM serie WHERE id = ?");
+        $state->execute([$id]);
+        return $state->fetchObject(Serie::class);
+    }
+
 
 
 }
