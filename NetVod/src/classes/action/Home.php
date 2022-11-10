36df6a91a5759cc3b1c5308757c3cfd8a35c88af
Mehdi Allali <mehdi.allali8@orange.fr>
2022-11-10 14:35:31 +0100

    rectification quand 0 favoris ou série en cours

--- NetVod/src/classes/action/Home.php
+++ NetVod/src/classes/action/Home.php
@@ -14,10 +14,23 @@ class Home extends Action
         try
         {
             $user = User::getFromSession();
-            $html = "<h2>Vos favoris :</h2>\n";
-            $html .= (new SeriesRenderer($user->getFavoritesSeries()))->render(2);
-            $html .= "<h2>Vos séries en cours :</h2>\n";
-            $html .= (new SeriesRenderer($user->getCurrentSeries()))->render(2);
+            $favoris = $user->getFavoritesSeries();
+            if (count($favoris)>0){
+                $html = "<h2>Vos favoris :</h2>\n";
+                $html .= (new SeriesRenderer($favoris))->render(2);
+            } else {
+                $html = "<h2>Vous n'avez pas encore de favoris</h2>\n";
+                $html .= "<p>Vous pouvez ajouter des séries à vos favoris en cliquant sur le bouton \"ajouter aux favoris\" dans la page d'une série</p>\n";
+            }
+            
+            $current = $user->getCurrentSeries();
+            if (count($current)>0){
+                $html .= "<h2>Vos séries en cours :</h2>\n";
+                $html .= (new SeriesRenderer($current))->render(2);
+            } else {
+                $html .= "<h2>Vous n'avez pas de série en cours</h2>\n";
+                $html .= "<p>Commencez à visionner une série pour qu'elle s'affiche ici</p>\n";
+            }
             return $html;
         }
         catch (AuthException $e)
